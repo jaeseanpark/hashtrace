337cc81c2eee4752576ee63a806989b713421cfb
administrator <jaeseanpark@gmail.com>
2021-06-14 13:27:09 +0900

    changes for hash.py removed comment

--- hash.py
+++ hash.py
@@ -21,16 +21,13 @@ while True:
 	readbytes = os.read(fd, buf)
 	if not readbytes:
 		break
-	"""
 	sha1hash = hashlib.sha1(readbytes)
 	sha1hashed = sha1hash.hexdigest()
 	sql = "insert into table1 (hash, blkno) values (%s, %s)"
 	val = (sha1hashed, count)
 	mycursor.execute(sql, val)
 	mydb.commit()
-	"""
 	if count % 10000 == 0:
 		print(count)
 
-print(count)
 os.close(fd)
