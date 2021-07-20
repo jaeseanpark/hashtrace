4542f40479a853d3535fda4bd86ad74b05c3a3da
administrator <jaeseanpark@gmail.com>
2021-07-20 15:39:43 +0900

    jul 20 backup

--- extract.py
+++ extract.py
@@ -27,76 +27,79 @@ fd1 = open("1.txt", "w")
 fd2 = open("2.txt", "w")
 fd3 = open("3.txt", "w")
 
-with open("dumpe2fs_1.txt", "r") as f:
+with open("dumpe2fs_2.txt", "r") as f:
   for line in tqdm(f.readlines()):
     if "superblock" in line or "bitmap" in line:
       fd1.write(line)
-    elif "Inode table" in line:
+    elif "Inode table" in line or "Reserved GDT" in line:
       fd2.write(line)
     elif "Free blocks:" in line:
       fd3.write(line)
+    else:
+      print(line)
+
+sys.exit(1)
 mylist = []
 with open("1.txt", "r") as f:
   for line in tqdm(f.readlines()):
     tmplist = [int(s) for s in re.findall(r'\b\d+\b', line)]
-    mylist.append(tmplist[0])
     if "descriptors" in line:
       mylist.extend(list(range(tmplist[1], tmplist[2] + 1)))
+    elif "bitmap" in line:
+      mylist.append(tmplist[0])
+    else:
+      print("1" + line)
 
 with open("2.txt", "r") as f:
   for line in tqdm(f.readlines()):
     tmplist = [int(s) for s in re.findall(r'\b\d+\b', line)]
+    if len(tmplist) < 2:
+      print(line)
+      continue
     mylist.extend(list(range(tmplist[0], tmplist[1] + 1)))
 
+freelist = []
 with open("3.txt", "r") as f:
   for line in tqdm(f.readlines()):
     tmplist = [int(s) for s in re.findall(r'\b\d+\b', line)]
     if len(tmplist) == 2:
       mylist.extend(list(range(tmplist[0], tmplist[1] + 1)))
+      freelist.extend(list(range(tmplist[0], tmplist[1] + 1)))
     elif not tmplist:
       continue
     elif len(tmplist) == 1:
       print(line)
       continue
-    elif len(tmplist) == 4:
-      print(str(len(tmplist)) + line)
-      mylist.extend(list(range(tmplist[0], tmplist[1] + 1)))
-      mylist.extend(list(range(tmplist[2], tmplist[3] + 1)))
-    elif len(tmplist) == 8:
-      print(str(len(tmplist)) + line)
-      mylist.extend(list(range(tmplist[0], tmplist[1] + 1)))
-      mylist.extend(list(range(tmplist[2], tmplist[3] + 1)))
-      mylist.extend(list(range(tmplist[4], tmplist[5] + 1)))
-      mylist.extend(list(range(tmplist[6], tmplist[7] + 1)))
-    elif len(tmplist) == 19:
-      print(str(len(tmplist)) + line)
-      mylist.append(tmplist[0])
-      mylist.extend(list(range(tmplist[1], tmplist[2] + 1)))
-      mylist.extend(list(range(tmplist[3], tmplist[4] + 1)))
-      mylist.extend(list(range(tmplist[5], tmplist[6] + 1)))
-      mylist.extend(list(range(tmplist[7], tmplist[8] + 1)))
-      mylist.extend(list(range(tmplist[9], tmplist[10] + 1)))
-      mylist.extend(list(range(tmplist[11], tmplist[12] + 1)))
-      mylist.extend(list(range(tmplist[13], tmplist[14] + 1)))
-      mylist.extend(list(range(tmplist[15], tmplist[16] + 1)))
-      mylist.extend(list(range(tmplist[17], tmplist[18] + 1)))
-
+    elif len(tmplist) > 2:
+      print(line)
+      i = 0
+      if len(tmplist) % 2 == 0:
+        while i < len(tmplist):
+          mylist.extend(list(range(tmplist[i], tmplist[i+1] + 1)))
+          freelist.extend(list(range(tmplist[i], tmplist[i+1] + 1)))
+          i += 2
+print(len(freelist))
 mylist.sort()
+print(len(mylist))
 mydata = []
 count = -1
-for num in tqdm(mylist):
-  count += 1
-  mytuple = (num,)
-  mydata.append(mytuple)
-  if count % 1000000 == 0 and count != 0:
-    add_hash(cur, mydata)
-    mydb.commit()
-    mydata.clear()
 
-if mydata:
-  add_hash(cur, mydata)
-  mydb.commit()
+#  for num in tqdm(mylist):
+#    count += 1
+#    mytuple = (num,)
+#    mydata.append(mytuple)
+#    if count % 1000000 == 0 and count != 0:
+#      add_hash(cur, mydata)
+#      mydb.commit()
+#      mydata.clear()
+#
+#  if mydata:
+#    add_hash(cur, mydata)
+#    mydb.commit()
 
 cur.close()
 mydb.close()
+fd1.close()
+fd2.close()
+fd3.close()
 
